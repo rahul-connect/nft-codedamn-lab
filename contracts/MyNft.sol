3da4f295b8489e900761c2a9c27cb92c30a442e1
Rahul <rahul.connect200@gmail.com>
2022-06-09 19:00:29 +0530

    first commit

---
+++ contracts/MyNft.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract MyNFT is ERC721URIStorage{
+
+}
